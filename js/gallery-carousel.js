d0bb496864915c3d411f9841573d568196ca8711
Iryna Shtynda <ira.shtynda@gmail.com>
2022-06-28 10:51:37 +0300

    added gallery-carousel js

---
+++ js/gallery-carousel.js
@@ -0,0 +1,50 @@
+(function () {
+
+    const slides = [
+        `<div><img src="img/gallery/gallery1.png" alt="gallery photo" /></div>`,
+        `<div><img src="img/gallery/gallery2.png" alt="gallery photo" /></div>`,
+        `<div><img src="img/gallery/gallery3.png" alt="gallery photo" /></div>`,
+        `<div><img src="img/gallery/gallery4.png" alt="gallery photo" /></div>`,
+        `<div><img src="img/gallery/gallery5.png" alt="gallery photo" /></div>`,
+        `<div><img src="img/gallery/gallery6.png" alt="gallery photo" /></div>`
+    ]
+
+    let slideIndex = 0;
+
+    function showCurrentSlide() {
+        const slideContainer = document.querySelector('.gallery-carousel .container-main .slider-container');
+        slideContainer.innerHTML = slides[slideIndex];
+        if (window.matchMedia('(min-width: 768px)').matches) {
+            const secondSlideIndex = slideIndex + 1 >= slides.length ? 0 : slideIndex + 1;
+            slideContainer.innerHTML += slides[secondSlideIndex];
+            if (window.matchMedia('(min-width: 992px)').matches) {
+                const thirdSlideIndex = secondSlideIndex + 1 >= slides.length ? 0 : secondSlideIndex + 1;
+                slideContainer.innerHTML += slides[thirdSlideIndex];
+                if (window.matchMedia('(min-width: 992px)').matches) {
+                    const fourthSlideIndex = thirdSlideIndex + 1 >= slides.length ? 0 : thirdSlideIndex + 1;
+                    slideContainer.innerHTML += slides[fourthSlideIndex];
+                }
+            }
+        }
+    }
+
+    function nextSlide() {
+        slideIndex = slideIndex + 1 >= slides.length ? 0 : slideIndex + 1;
+        showCurrentSlide();
+    }
+
+    function prevSlide() {
+        slideIndex = slideIndex - 1 < 0 ? slides.length - 1 : slideIndex - 1;
+        showCurrentSlide();
+    }
+
+    showCurrentSlide();
+
+    const nextButton = document.querySelector('#gallery > div > button.btn-next');
+    nextButton.addEventListener('click', nextSlide);
+
+    const prevButton = document.querySelector('#gallery > div > button.btn-prev');
+    prevButton.addEventListener('click', prevSlide);
+
+    window.addEventListener('resize', showCurrentSlide);
+})();
