ce65c98c9deaf04617815f9d36870f7ae98dd0bb
TatianaBonat <kaospbv@gmail.com>
2022-07-13 20:36:28 +0000

    factored btn on click style

--- js/btn-advice.js
+++ js/btn-advice.js
@@ -1,59 +1,15 @@
-
 const btnConsultation = document.getElementById('btnConsultation');
 const bgc = btnConsultation.style.backgroundColor;
 const txtColor = btnConsultation.style.color;
 
 btnConsultation.addEventListener('click', function(e) {
-    e.target.style.backgroundColor = "white";
-    e.target.style.color = "black";
+    const element = e.target.style;
+    element.backgroundColor = "white";
+    element.color = "black";
 
     setTimeout(function() {
-        e.target.style.backgroundColor = bgc;
-        e.target.style.color = txtColor;
+        element.backgroundColor = bgc;
+        element.color = txtColor;
     }, 1500);
-    
 
 });
-
-
-// function changeBgc(e) {
-//     e.target.style.backgroundColor = 'white';
-// }
-
-
-
-
-
-
-
-
-// вот так сработает
-// document.querySelector('button').onclick = function(){
-//     console.log(this)  // теперь this это кнопка
-//     this.style.background = "red";
-//   }
-  
-    
-
-
-
-// function clickColor() {
-// 	var d;
-// 	d = document.getElementById('tagP');
-
-// 	if (d.style.color='red') {
-// 		d.style.color='black';
-// 	}
-
-// 	if (d.style.color='black') {
-// 		d.style.color='red';
-// 	}
-
-// 	console.log(d.style.color);
-// };
-
-// btnConsultation.addEventListener('click', callback);
-// function callback() {
-//     btnConsultation.style.backgroundColor = 'white';
-// }
-
